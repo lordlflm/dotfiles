46fed211bbd337402d87aed3d895c2e867f8358e
lflm <thomdlaf@gmail.com>
2024-10-15 13:57:57 +0000

    cahnges

--- dot_script/install_golang.sh
+++ dot_script/install_golang.sh
@@ -4,6 +4,7 @@ GO_VERSION="1.23.2"
 
 echo "Installing Golang..."
 wget "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+echo "Unpacking..."
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
 rm "go${GO_VERSION}.linux-amd64.tar.gz"
 echo "Done."
