77fbb1e159f41b9902d1edbdf4a4407f34f9f3b6
lflm <thomdlaf@gmail.com>
2024-10-17 17:11:38 +0000

    fix node install

--- dot_script/install_node.sh
+++ dot_script/install_node.sh
@@ -6,6 +6,9 @@ r=${r:-n}
 if [[ "$r" == "y" ]]; then
   echo "Installing pwntools..."
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   nvm install node
   echo "Done."
 else
